7b5d91387a38bef5076a0e7a1875ae578c679762
Josh Siegel <jsiegel@jumptrading.com>
2021-12-09 11:09:08 +0000

    algorand: toy deploy @ gusc1a-ossdev-jsl1
    Change-Id: Ic2dbaf8c3dbd1675d3bad90c9de04072ce5bcd83

--- algorand/deploy.py
+++ algorand/deploy.py
@@ -6,3 +6,32 @@ from pyteal import compileTeal, Mode, Expr
 from pyteal import *
 from algosdk.logic import get_application_address
 
+ALGOD_ADDRESS = "http://localhost:4001"
+ALGOD_TOKEN = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+client = AlgodClient(ALGOD_TOKEN, ALGOD_ADDRESS)
+
+vaa_processor_approval = client.compile(open("vaa-processor-approval.teal").read())
+vaa_processor_clear = client.compile(open("vaa-processor-clear.teal").read())
+vaa_verify = client.compile(open("vaa-verify.teal").read())
+
+globalSchema = transaction.StateSchema(num_uints=2, num_byte_slices=6)
+localSchema = transaction.StateSchema(num_uints=0, num_byte_slices=0)
+
+txn = transaction.ApplicationCreateTxn(
+    sender=sender.getAddress(),
+    on_complete=transaction.OnComplete.NoOpOC,
+    approval_program=vaa_processor_approval,
+    clear_program=vaa_processor_clear,
+    global_schema=globalSchema,
+    local_schema=localSchema,
+    app_args=app_args,
+    sp=client.suggested_params(),
+)
+
+signedTxn = txn.sign(sender.getPrivateKey())
+        
+client.send_transaction(signedTxn)
+        
+response = self.waitForTransaction(client, signedTxn.get_txid())
+
